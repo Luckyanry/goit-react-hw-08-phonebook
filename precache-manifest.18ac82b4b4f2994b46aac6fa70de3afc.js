614c592ab441b424aaaecec229fc779db4302738
Andrii Kochmaruk <luckyanry@gmail.com>
2020-08-09 21:16:22 +0200

    Updates

---
+++ precache-manifest.18ac82b4b4f2994b46aac6fa70de3afc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ef6bd9e6c4345ca419fee80a7bf3e6",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "7ca86af787226fe4e5ed",
+    "url": "/goit-react-hw-08-phonebook/static/css/3.a0a182a1.chunk.css"
+  },
+  {
+    "revision": "2602b5168081c3083fd9",
+    "url": "/goit-react-hw-08-phonebook/static/css/4.a6f9d388.chunk.css"
+  },
+  {
+    "revision": "f2035afc1268b7c76c67",
+    "url": "/goit-react-hw-08-phonebook/static/css/5.d495050a.chunk.css"
+  },
+  {
+    "revision": "9ce913e43ae222ead06e",
+    "url": "/goit-react-hw-08-phonebook/static/css/7.72821b1c.chunk.css"
+  },
+  {
+    "revision": "f2d4bc87c7987c5f0189",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.ac8c0b0a.chunk.css"
+  },
+  {
+    "revision": "90f6de191b777f8c28e5",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.52ed71d0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.52ed71d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca86af787226fe4e5ed",
+    "url": "/goit-react-hw-08-phonebook/static/js/3.a4013668.chunk.js"
+  },
+  {
+    "revision": "2602b5168081c3083fd9",
+    "url": "/goit-react-hw-08-phonebook/static/js/4.e839b02f.chunk.js"
+  },
+  {
+    "revision": "f2035afc1268b7c76c67",
+    "url": "/goit-react-hw-08-phonebook/static/js/5.65d24a4d.chunk.js"
+  },
+  {
+    "revision": "89bd8239030d3771886f",
+    "url": "/goit-react-hw-08-phonebook/static/js/6.0cf85f1f.chunk.js"
+  },
+  {
+    "revision": "9ce913e43ae222ead06e",
+    "url": "/goit-react-hw-08-phonebook/static/js/7.d50b2615.chunk.js"
+  },
+  {
+    "revision": "f2d4bc87c7987c5f0189",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.67f79e24.chunk.js"
+  },
+  {
+    "revision": "e88ebf3b0d8325998168",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.5ebe9c40.js"
+  }
+]);
---
+++ static/js/7.d50b2615.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[7],{117:function(e,t,o){},125:function(e,t,o){"use strict";o.r(t);var n=o(0),a=o.n(n);o(117);t.default=function(){return a.a.createElement("div",null,a.a.createElement("h1",{className:"Home-title"},"Welcome to phonebook application!"))}}}]);
+//# sourceMappingURL=7.d50b2615.chunk.js.map
